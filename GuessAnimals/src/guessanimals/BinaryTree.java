2bb4e387571c537f2c70bf9f760156721b0d4388
jmiguelzin <miguelarkjow@gmail.com>
2019-11-02 17:41:47 -0300

    adicionando funções básicas

--- GuessAnimals/src/guessanimals/BinaryTree.java
+++ GuessAnimals/src/guessanimals/BinaryTree.java
@@ -2,11 +2,14 @@
 package guessanimals;
 
 public class BinaryTree {
+    
     private Node root;
     
     
-    public BinaryTree(){
-        root = null;
+    public BinaryTree(String info){
+        Node newRoot = new Node(info);
+        this.root = newRoot;
+        
     }
 
     public Node getRoot() {
@@ -17,5 +20,38 @@ public class BinaryTree {
         this.root = root;
     }
     
+    public Node insertNodeLeft(Node node, String info){
+        
+        if (node.getLeft() == null){
+            Node newNode = new Node(info);
+            node.setLeft(newNode);
+            return newNode;
+        }
+        
+        return null;
+        
+    }   
     
+    public Node insertNodeRight(Node node, String info){
+        
+        if (node.getRight() == null){
+            Node newNode = new Node(info);
+            node.setRight(newNode);
+            return newNode;
+        }
+        
+        return null;
+        
+    }    
+    
+    public void startGame(Node root){
+        
+    }
+    
+    public void loadGame(){
+        BinaryTree tree = new BinaryTree("vive na água");
+        insertNodeLeft(tree.getRoot(), "Macaco");
+        insertNodeRight(tree.getRoot(), "Peixe");
+        startGame(tree.root);
+    }
 }
